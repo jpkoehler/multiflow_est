d6afe231879fe82a152ffc54e05b0ee8a24d1385
MatheusDelduque <matheusdelduque1@gmail.com>
2024-01-31 16:17:29 -0300

    refactor: limpado o codigo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import utils as utl
 
 # Custom imports
 from multipage import MultiPage
-from pages import equipment # import your pages here
+from pages import equipment  # import your pages here
 
 # Create an instance of the app
 app = MultiPage()
@@ -13,20 +13,7 @@ app = MultiPage()
 # st.title("Field Design")
 utl.local_css("style.css")
 
-# Add all your applications (pages) here
-# app.add_page("Características do Projeto", oilproperties.app)
-# app.add_page("Reservatório e Escoamento", reservoir_flow.app)
-# app.add_page("Subsea e Naval", subsea.app)
-# app.add_page("Poços", wells.app)
 app.add_page("Chen Correlation", equipment.app)
-# app.add_page("Planta de Processo - Arranjo", layout.app)
-# app.add_page("Análise de Risco", risk.app)
-# app.add_page("Análise Econômica", economic.app)
-# app.add_page("SMS", SMS.app)
-# app.add_page("Resultados", results.app)
-
-
-
 
 # The main app
 app.run()
