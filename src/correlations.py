f80ade4b98a1a789994521894494ac7e13146608
jpkoehler <102050604+jpkoehler@users.noreply.github.com>
2024-09-08 22:03:56 -0300

    Update correlations.py

--- src/correlations.py
+++ src/correlations.py
@@ -21,7 +21,7 @@ def app():
     L = st.number_input('Length:', 0.0, 10000.0,  2000.0, step=0.1)
     depth = st.number_input('Depth:', 0.0, 10000.0,  1000.0, step=0.1)
 
-    button1 = st.button('Calculate')
+    button1 = st.button('Calculate Chen')
 
     if st.session_state.get('button1') != True:
         st.session_state['button1'] = button1
@@ -30,7 +30,7 @@ def app():
         with st.spinner('Processando...'):
             chen_result = chen_flow(IP, Pe, Psep, diam, L, depth)
 
-            beggs_and_brill_result = beggs_brill_flow(IP, Pe, Psep, diam, L, depth, RGO)
+           """ beggs_and_brill_result = beggs_brill_flow(IP, Pe, Psep, diam, L, depth, RGO) """
 
             """ hagedorn_brow_result = hagedorn_brown_flow(
                 IP, Pe, Psep, diam, L, depth, RGO) """
@@ -43,14 +43,14 @@ def app():
             dataknock.index += 1
             st.table(dataknock)
 
-            dataknock = pd.DataFrame(
+            """ dataknock = pd.DataFrame(
                 [["Wellhead Pressure (bara)", str("{:.0f}".format(beggs_and_brill_result[0]))],
                  ["Well Prod (Barrels/day)", str(
                      "{:.0f}".format(beggs_and_brill_result[1]))],
                  ],
                 columns=['Beggs and Brill', 'Value'])
             dataknock.index += 1
-            st.table(dataknock) 
+            st.table(dataknock) """
 
             """ dataknock = pd.DataFrame(
                 [["PW Calc", str("{:.0f}".format(hagedorn_brow_result[0]))],
@@ -60,5 +60,23 @@ def app():
                 columns=['Hagedorn Brown', 'Value'])
             dataknock.index += 1
             st.table(dataknock) """
+            
+    button2 = st.button('Calculate H&B')
+
+    if st.session_state.get('button2') != True:
+        st.session_state['button2'] = button1
+
+    if (button2):
+        with st.spinner('Processando...'):
+            beggs_and_brill_result = beggs_brill_flow(IP, Pe, Psep, diam, L, depth, RGO)
+
+            dataknock = pd.DataFrame(
+                [["Wellhead Pressure (bara)", str("{:.0f}".format(beggs_and_brill_result[0]))],
+                 ["Well Prod (Barrels/day)", str(
+                     "{:.0f}".format(beggs_and_brill_result[1]))],
+                 ],
+                columns=['Beggs and Brill', 'Value'])
+            dataknock.index += 1
+            st.table(dataknock) 
 
         st.success('Sucesso!')
